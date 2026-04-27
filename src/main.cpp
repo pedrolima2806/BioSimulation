985cb60aec0398e3c1d1630b57ec18e69a0a9762
pedrolima2806 <pedrolimasilva2003@gmail.com>
2026-04-27 15:43:03 -0300

    fluxo inicial

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,94 @@
 #include <iostream>
+#include <chrono>
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
 
 int main() {
+    //Initialization
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    if (!MIX_Init()) {
+        std::cerr << "MIX_Init failed." << std::endl;
+    }
+    if (!TTF_Init()) {
+        std::cerr << "TTF_Init failed." << std::endl;
+    }
 
+    //Window e renderer
+    int width = 1536, height = 864;
+    SDL_Window *window = SDL_CreateWindow("hellcat_mp", width, height, 0);
+    if (!window) {
+        std::cerr << "Failed to create window:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer:" << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    //Mixer device
+    MIX_Mixer *mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr);
+    if (!mixer) {
+        std::cerr << "Failed to open audio device:" << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    //Font
+    TTF_Font *font = TTF_OpenFont("../assets/fonts/Quantico/Quantico-Regular.ttf", 16);
+    if (!font) {
+        std::cerr << "Failed to open font:" << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    //Loop cycle
+    auto previousTime = std::chrono::steady_clock::now();
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT ||
+                event.type == SDL_EVENT_KEY_DOWN && event.key.scancode == SDL_SCANCODE_ESCAPE) {
+                running = false;
+                }
+        }
+        //outside handleEvent
+        //background
+        SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
+        SDL_RenderClear(renderer);
+
+        //deltaMs
+        auto currentTime = std::chrono::steady_clock::now();
+        double deltaMs = std::chrono::duration<double, std::milli>(currentTime - previousTime).count();
+        previousTime = currentTime;
+
+        //update
+
+        //rendering
+        SDL_RenderPresent(renderer);
+    }
+
+
+    //Finalization
+    MIX_DestroyMixer(mixer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    MIX_Quit();
+    SDL_Quit();
     return 0;
 }
