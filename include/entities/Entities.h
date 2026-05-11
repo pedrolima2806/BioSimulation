921f018dda17ff54744f33e66c281192e1110776
pedrolima2806 <pedrolimasilva2003@gmail.com>
2026-05-11 12:05:18 -0300

    Adicionado gerador e movimento de entidades

---
+++ include/entities/Entities.h
@@ -0,0 +1,13 @@
+//
+// Created by pedro on 5/11/26.
+//
+
+#ifndef BIOSIMULATION_ENTITIES_H
+#define BIOSIMULATION_ENTITIES_H
+
+
+class Entities {
+};
+
+
+#endif //BIOSIMULATION_ENTITIES_H
---
+++ src/entities/Entities.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pedro on 5/11/26.
+//
+
+#include "Entities.h"
